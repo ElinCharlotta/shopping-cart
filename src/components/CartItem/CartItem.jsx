b300a2552ef26025c175108635ae02a53a90c32a
ElinCharlotta <janssonelin@hotmail.com>
2024-04-04 10:40:19 +0200

    added code
    Made improvements to increase and decrease

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,34 +2,42 @@ import './CartItem.css';
 import arrowUp from '../../assets/arrow-up.svg';
 import arrowDown from '../../assets/arrow-down.svg';
 import { useDispatch } from 'react-redux';
-import { decreaseQuantity, increaseQuantity } from '../../reducers/cartReducer';
+import { decreaseQuantity, increaseQuantity, removeProduct } from '../../reducers/cartReducer';
 
 function CartItem(props) {
   const { item } = props;
   const dispatch = useDispatch();
-  const handleIncreaseQuantity = () => {
-    dispatch(increaseQuantity(item));
-  }
-  const handleDecreaseQuantity = () => 
-  { dispatch(decreaseQuantity(item)); }
 
   return (
     <article className='cart-item'>
       <img src={item.thumbnail} alt={item.title} className='cart-item-image' />
 
       <h3 className='cart-item-title'>
-    {item.title}
+        {item.title}
         <span className='dots'></span>
       </h3>
-      <p className='cart-item-desc'>{item.price} kr</p>
+      <p className='cart-item-desc'>{item.price * item.quantity} kr</p>
       <aside className='item-quantity'>
-        <img src={arrowUp} alt='increase' onClick={handleIncreaseQuantity} />{' '}
+        <img src={arrowUp} alt='increase' 
+        onClick={()=> 
+        dispatch(increaseQuantity(item.id))} />
+        {' '}
         { /** Här ska du lägga till en onclick på img-elementet för att kunna öka kvantitet. När man klickar på knappen ska du göra en dispatch(increaseQuantity()) till din redux store.  */}
         <p className='quantity'>{item.quantity}</p>{' '}
         {/** Här vill du ersätta så att du läser in nuvarande "quantity" istället */}
-        <img src={arrowDown} alt='decrease' onClick={handleDecreaseQuantity} />{' '}
+        <img
+  src={arrowDown}
+  alt='decrease'
+  onClick={() => {
+    if (item.quantity > 0) {
+      dispatch(decreaseQuantity(item.id));
+    } else {
+      dispatch(removeProduct(item.id));
+    }
+  }}
+/>
         {/** Här ska du lägga till en onclick på img-elementet för att kunna minska kvantitet. När man klickar på knappen ska du göra en dispatch(decreaseQuantity()) till din redux store.  */}
-      
+
       </aside>
     </article>
   );
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -12,32 +12,34 @@ const cartSlice = createSlice({
         existingProduct.quantity += 1;
       } else {
         state.push(action.payload);
-        //** Här ska du kolla om produkten redan finns i arrayen och ifall den finns ska du inte pusha in i array:en utan istället öka egenskapen "quantity" med 1. Ifall produkten inte finns ska du pusha in produkten i array:en. Kolla i Product.jsx hur objektet för en produkt ser ut, där ser du egenskapen "quantity". */
-
       }
     },
-    //* Här ska du lägga till två reducers "increaseQuantity" och "decreaseQuantity" som ökar eller minska "quantity" med 1. Ifall quantity är 1 och man minska ska produkten tas bort från array:en. Det är dessa två som kopplas till knapparna i CartItem.jsx, se kommentarer där. */
-
     increaseQuantity(state, action) {
-      const product = state.find(product => product.id === action.payload.id);
-      if (product) {
-        product.quantity += 1;
-      }
+      state.map(cartItem => {
+        if (cartItem.id === action.payload) {
+          cartItem.quantity++;
+        }
+        return cartItem;
+      });
     },
     decreaseQuantity(state, action) {
-      const productFound = state.findIndex(product => product.id === action.payload.id);
-      if (productFound !== -1) {
-        const product = state[productFound];
-        if (product.quantity === 1) {
-          state.splice(productFound, 1); // Remove product if quantity is 1
-        } else {
-          product.quantity -= 1;
+      state.map(cartItem => {
+        if (cartItem.id === action.payload) {
+          cartItem.quantity--;
         }
-      }
+        return cartItem;
+      });
     },
-  },
+    removeProduct(state,action){
+      const newCart = state.filter((cartItem)=> {
+        if (cartItem.id === action.payload) {
+          return cartItem
+        }
+      });
+    }
+  }
 });
 
-export const { addToCart, increaseQuantity, decreaseQuantity } = cartSlice.actions;
+export const { addToCart, increaseQuantity, decreaseQuantity, removeProduct } = cartSlice.actions;
 
 export default cartSlice.reducer;
